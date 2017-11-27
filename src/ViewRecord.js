bd945551531b8a2bd32d09b89383ade7e51470e2
Mike Taylor <mike@indexdata.com>
2017-11-27 16:24:18 +0000

    New, skeletal, full-record view

---
+++ src/ViewRecord.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class ViewRecord extends React.Component {
+  render() {
+    return <p>Viewing full record</p>;
+  }
+}
+
+export default ViewRecord;
