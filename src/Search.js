d99d4adaf6622feb5578ee76bee76d1af9c6a348
Mike Taylor <mike@indexdata.com>
2017-11-27 16:25:59 +0000

    First sort-of-working version
    Now successfully searching and sorting in the Items service (while I
    wait for the mocked Codex service), but filtering is not yet working:
    when any filter is enabled I find zero records. Also not yet working:
    view full record.

--- src/Search.js
+++ src/Search.js
@@ -1,9 +1,73 @@
+import _ from 'lodash';
 import React from 'react';
+import makeQueryFunction from '@folio/stripes-components/util/makeQueryFunction';
+import SearchAndSort from '@folio/stripes-smart-components/lib/SearchAndSort';
+import ViewRecord from './ViewRecord';
+import packageInfo from '../package';
+
+const filterConfig = [
+  {
+    label: 'Material Types',
+    name: 'item',
+    cql: 'materialType.id',
+    values: [
+      { name: 'Book', cql: 'book' },
+      { name: 'DVD', cql: 'dvd' },
+    ],
+  },
+];
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Search extends React.Component {
+  static manifest = Object.freeze({
+    resultCount: { initialValue: 30 },
+    query: { initialValue: {} },
+    records: {
+      type: 'okapi',
+      records: 'items',
+      path: 'inventory/items',
+      recordsRequired: '%{resultCount}',
+      perRequest: 30,
+      GET: {
+        params: {
+          query: makeQueryFunction(
+            'cql.allRecords=1',
+            'materialType.name="$QUERY" or barcode="$QUERY*" or title="$QUERY*"',
+            { 'Material Type': 'materialType.name' },
+            filterConfig,
+          ),
+        },
+        staticFallback: { params: {} },
+      },
+    },
+  });
+
   render() {
-    return <p>Hello, world!</p>;
+    const props = this.props;
+
+    const resultsFormatter = {
+      'Material Type': x => _.get(x, ['materialType', 'name']),
+      status: x => _.get(x, ['status', 'name']) || '--',
+    };
+
+    return (<SearchAndSort
+      moduleName={packageInfo.name.replace(/.*\//, '')}
+      moduleTitle={packageInfo.stripes.displayName}
+      objectName="record"
+      baseRoute={packageInfo.stripes.route}
+      initialPath={(_.get(packageInfo, ['stripes', 'home']) ||
+                    _.get(packageInfo, ['stripes', 'route']))}
+      filterConfig={filterConfig}
+      initialResultCount={30}
+      resultCountIncrement={30}
+      viewRecordComponent={ViewRecord}
+      visibleColumns={['Material Type', 'barcode', 'title', 'status']}
+      resultsFormatter={resultsFormatter}
+      viewRecordPerms="users.item.get"
+      disableRecordCreation
+      parentResources={props.resources}
+      parentMutator={props.mutator}
+    />);
   }
 }
 
