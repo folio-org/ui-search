9772d70ac5c7434e46aa8fc1f3bbc6ce4bcaf82f
Mike Taylor <mike@indexdata.com>
2018-02-14 16:56:43 +0000

    Tweak test name

--- test/ui-testing/null.js
+++ test/ui-testing/null.js
@@ -1,7 +1,7 @@
 const { describe, it } = require('mocha');
 
 module.exports.test = (context) => {
-  describe('Testing apparatus works', function nullTest() {
+  describe('Testing apparatus', function nullTest() {
     const { config } = context;
 
     this.timeout(Number(config.test_timeout));
