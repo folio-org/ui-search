500528c0ea7d1c0c56597ac1bd9233c77a76f392
Mike Taylor <mike@indexdata.com>
2018-02-15 10:30:01 +0000

    Include title-search test

--- test/ui-testing/test.js
+++ test/ui-testing/test.js
@@ -1,6 +1,7 @@
-const nullTest = require('./null.js');
+const nullTest = require('./01-null.js');
+const titleSearchTest = require('./02-titleSearch.js');
 
 module.exports.test = (context) => {
   nullTest.test(context);
-  // ... and even more exciting tests to follow!
+  titleSearchTest.test(context);
 };
