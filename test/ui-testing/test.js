3ed1d9cd233f28549909b19c11f0b066af01392b
Mike Taylor <mike@indexdata.com>
2018-02-15 16:51:36 +0000

    Do not attempt obsolete "codex" test

--- test/ui-testing/test.js
+++ test/ui-testing/test.js
@@ -1,9 +1,7 @@
 const nullTest = require('./01-null.js');
 const titleSearchTest = require('./02-titleSearch.js');
-const codex = require('./codex.js');
 
 module.exports.test = (context) => {
   nullTest.test(context);
   titleSearchTest.test(context);
-  codex.test(context);
 };
