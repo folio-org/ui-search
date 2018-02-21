86d3ae7071e5dcad35bf47da025769e7740f26e7
Mike Taylor <mike@indexdata.com>
2018-02-21 22:50:36 +0000

    Move source-specific redirect recipes into functions

--- src/redirectParams.js
+++ src/redirectParams.js
@@ -1,26 +1,34 @@
 import _ from 'lodash';
 
+function redirectParamsKB(record, resources) {
+  const obj = {};
+  obj._path = `/eholdings/titles/${record.id}`;
+  obj.searchType = 'titles';
+  obj.q = _.get(resources, ['query', 'query']);
+  obj.query = null;
+  // TODO: reseting filters is a temp solution until
+  // https://issues.folio.org/browse/UISE-67 is fixed
+  obj.filters = '';
+  return obj;
+}
+
+function redirectParamsLocal(record, _resources) {
+  const obj = {};
+  obj._path = `/inventory/view/${record.id}`;
+  obj.searchType = null;
+  obj.q = null;
+  // The 'query' parameter should already be correct
+  // TODO: reseting filters is a temp solution until
+  // https://issues.folio.org/browse/UISE-67 is fixed
+  obj.filters = '';
+  return obj;
+}
+
 function redirectParams(record, resources) {
   if (record.source === 'kb') {
-    const obj = {};
-    obj._path = `/eholdings/titles/${record.id}`;
-    obj.searchType = 'titles';
-    obj.q = _.get(resources, ['query', 'query']);
-    obj.query = null;
-    // TODO: reseting filters is a temp solution until
-    // https://issues.folio.org/browse/UISE-67 is fixed
-    obj.filters = '';
-    return obj;
+    return redirectParamsKB(record, resources);
   } else if (record.source === 'local') {
-    const obj = {};
-    obj._path = `/inventory/view/${record.id}`;
-    obj.searchType = null;
-    obj.q = null;
-    // The 'query' parameter should already be correct
-    // TODO: reseting filters is a temp solution until
-    // https://issues.folio.org/browse/UISE-67 is fixed
-    obj.filters = '';
-    return obj;
+    return redirectParamsLocal(record, resources);
   }
   return null;
 }
