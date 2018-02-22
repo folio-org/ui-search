43633f32de3c33cce34b7452742f72490e728de6
Mike Taylor <mike@indexdata.com>
2018-02-22 00:47:36 +0000

    Add mapFilters function and config for Inventory

--- src/redirectParams.js
+++ src/redirectParams.js
@@ -1,4 +1,48 @@
 import _ from 'lodash';
+import { filterState } from '@folio/stripes-components/lib/FilterGroups';
+
+
+// 'filters' is a comma-separated list of 'FILTER.VALUE'
+//  e.g. 'available.Available online,source.Local,type.Books,type.eBooks'
+//
+// 'mapping' is an object specifying how to map filters:
+//  keys are 'name' elements of objects in filterConfig
+//  values may be:
+//   null (when the filter has no equivalent in the destination app and should be discarded)
+//   otherwise an object with keys:
+//    name: the name of the filter in the destination app
+//    values: may be either
+//     null (when the values are the same in the destination app)
+//     or a map of values in Codex search to corresponding values in destination app
+//
+function mapFilters(filters, mapping) {
+  const filterKeys = filterState(filters);
+  const acc = [];
+
+  Object.keys(filterKeys).forEach((fullName) => {
+    const [groupName, fieldName] = fullName.split('.');
+    console.log(`considering '${groupName}' filter '${fieldName}'`);
+    const group = mapping[groupName];
+    if (group) {
+      if (!group.values) {
+        // The values do not need mapping
+        const mapped = `${group.name}.${fieldName}`;
+        acc.push(mapped);
+      } else {
+        const mappedVal = group.values[fieldName];
+        if (mappedVal) {
+          const mapped = `${group.name}.${mappedVal}`;
+          acc.push(mapped);
+        }
+      }
+    }
+  });
+
+  const res = acc.join(',');
+  console.log(`mapped '${filters}' -> '${res}'`);
+  return res;
+}
+
 
 function redirectParamsKB(record, resources) {
   const obj = {
@@ -7,28 +51,62 @@ function redirectParamsKB(record, resources) {
     searchType: 'titles',
     q: _.get(resources, ['query', 'query']),
     query: null,
+    filters: mapFilters(_.get(resources, ['query', 'filters']), {
+      // add config
+    }),
   };
-
-  // TODO: reseting filters is a temp solution until
-  // https://issues.folio.org/browse/UISE-67 is fixed
-  obj.filters = '';
 
   return obj;
 }
 
-function redirectParamsLocal(record, _resources) {
+
+function redirectParamsLocal(record, resources) {
   const obj = {
     _path: `/inventory/view/${record.id}`,
     qindex: null,
     // The 'query' parameter takes the same value in inventory as here
+    filters: mapFilters(_.get(resources, ['query', 'filters']), {
+      source: null,
+      type: {
+        name: 'resource',
+        values: {
+          /* eslint-disable quote-props */
+          'Audio': 'Music (Audio)',
+          'Audiobooks': null,
+          'Books': 'Books',
+          'Bookseries': null,
+          'Databases': null,
+          'eBooks': 'eBooks',
+          'Kits': 'Kits',
+          'Maps': 'Maps',
+          'Music': 'Music (Scores)',
+          'Newspapers': null,
+          'Newsletters': null,
+          'Periodicals': 'Serials',
+          'Posters': 'Charts Posters',
+          'Reports': null,
+          'Proceedings': null,
+          'Thesis and Dissertation': 'Theses',
+          'Unspecified': null,
+          'Video': 'Videorecording',
+          'Web Resources': 'Web Resources',
+        },
+      },
+      location: {
+        name: 'location',
+        values: null,
+      },
+      available: null,
+      lang: {
+        name: 'language',
+        values: null,
+      },
+    }),
   };
-
-  // TODO: reseting filters is a temp solution until
-  // https://issues.folio.org/browse/UISE-67 is fixed
-  obj.filters = '';
 
   return obj;
 }
+
 
 function redirectParams(record, resources) {
   if (record.source === 'kb') {
@@ -39,5 +117,6 @@ function redirectParams(record, resources) {
     return null;
   }
 }
+
 
 export default redirectParams;
