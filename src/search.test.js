f21953f6123409d2f1b40885fe3aeccbc5491f48
Vlad Samets <uladislau_samets@epam.com>
2021-10-20 15:57:49 +0300

    more tests

--- src/search.test.js
+++ src/search.test.js
@@ -8,26 +8,6 @@ import buildStripes from '../test/jest/__mock__/stripesCore.mock';
 
 import Routing from './index';
 
-jest.mock('@folio/stripes-core', () => ({
-  ...jest.requireActual('@folio/stripes-core'),
-  withStripes : Component => ({ ...rest }) => {
-    const fakeStripes = {
-      connect: component => component,
-      hasPerm: () => true,
-      logger: {
-        log: jest.fn(),
-      }
-    };
-
-    return (
-      <Component
-        {...rest}
-        stripes={fakeStripes}
-      />
-    );
-  },
-}));
-
 jest.mock('react-virtualized-auto-sizer', () => ({ children }) => children({ width: 1920, height: 1080 }));
 
 const stripes = buildStripes();
@@ -35,34 +15,52 @@ const match = {
   path:'/codexsearch',
 };
 
-const records = [{
-  id : '1b74ab75-9f41-4837-8662-a1d99118008d',
-  title : 'A journey through Europe Bildtontraeger high-speed lines European Commission, Directorate-General for Mobility and Transport',
-  contributor : [{
-    type : 'Corporate name',
-    name : 'Europäische Kommission Generaldirektion Mobilität und Verkehr'
-  }],
-  subject : [],
-  type : 'unspecified',
-  identifier : [{
-    value : '(DE-599)GBV643935371',
-    type : 'System control number'
-  }, {
-    value : '10.2768/21035',
-    type : 'Other standard identifier'
-  }, {
-    value : '643935371',
-    type : 'Control number'
-  }, {
-    value : 'MI-32-10-386-57-Z',
-    type: 'Publisher or distributor number'
-  }, {
-    value: '9789279164316',
-    type: 'ISBN'
-  }],
-  'source' : 'kb',
-  'language' : ['ger', 'eng', 'spa', 'fre', 'ita', 'dut', 'por']
-},
+const records = [
+  {
+    id: '1',
+    title: 'Record title 1',
+    contributor: [{
+      type: 'Corporate name',
+      name: 'Europäische Kommission Generaldirektion Mobilität und Verkehr'
+    }],
+    subject: [],
+    type: 'unspecified',
+    identifier: [{
+      value: '(DE-599)GBV643935371',
+      type: 'System control number'
+    }],
+    'source': 'local',
+  },
+  {
+    id: '2',
+    title: 'Record title 2',
+    contributor: [{
+      type: 'Corporate name',
+      name: 'Europäische Kommission Generaldirektion Mobilität und Verkehr'
+    }],
+    subject: [],
+    type: 'unspecified',
+    identifier: [{
+      value: '(DE-599)GBV643935371',
+      type: 'System control number'
+    }],
+    'source': 'kb',
+  },
+  {
+    id: '3',
+    contributor: [{
+      type: 'Corporate name',
+      name: 'Europäische Kommission Generaldirektion Mobilität und Verkehr'
+    }],
+    subject: [],
+    type: 'unspecified',
+    identifier: [{
+      value: '(DE-599)GBV643935371',
+      type: 'System control number'
+    }],
+    'source': 'local',
+  },
+
 ];
 
 const resources = {
@@ -76,7 +74,7 @@ const resources = {
     records,
     hasLoaded: true,
     other: {
-      totalRecords: 1
+      totalRecords: 3
     }
   }
 };
@@ -95,10 +93,46 @@ const mutator = {
   }
 };
 
-const renderRouting = (showSettings) => {
-  window.history.pushState({}, 'Test page', '/codexsearch');
+jest.mock('@folio/stripes-core', () => ({
+  ...jest.requireActual('@folio/stripes-core'),
+  withStripes: Component => ({ ...rest }) => {
+    const fakeStripes = {
+      connect: ConnectedComponent => ({ ...restProps }) => (
+        <ConnectedComponent
+          resources={
+            {
+              record: {
+                records,
+                hasLoaded: true,
+                other: {
+                  totalRecords: 3
+                }
+              }
+            }
+          }
+          {...restProps}
+        />
+      ),
+      hasPerm: () => true,
+      logger: {
+        log: jest.fn(),
+      }
+    };
+
+    return (
+      <Component
+        {...rest}
+        stripes={fakeStripes}
+      />
+    );
+  },
+}));
+
+const renderRouting = (showSettings, route) => {
+  const path = ['/codexsearch', route].join('');
+
   return render(
-    <MemoryRouter initialEntries={['/codexsearch']}>
+    <MemoryRouter initialEntries={[path]}>
       <Routing
         match={match}
         stripes={stripes}
@@ -122,25 +156,37 @@ describe('Routing', () => {
 
     const inputSearch = screen.getByRole('searchbox', { name: /searchFieldLabel/ });
     const localCheckbox = screen.getByRole('checkbox', { name: /sources.local/ });
+    const indexSelect = screen.getByRole('combobox', { name: /searchFieldIndex/ });
+    // const titleOption = screen.getByRole('option', { name: /searchableIndexes.subject/ });
 
+
+    // user.click(indexSelect);
+    // user.click(titleOption);
+    user.selectOptions(indexSelect, 'ui-search.searchableIndexes.identifier');
     user.type(inputSearch, 'test');
     user.click(localCheckbox);
 
-    expect(screen.getByRole('gridcell', { name: /A journey through Europe/ })).toBeVisible();
+    expect(screen.getByRole('gridcell', { name: 'Record title 1' })).toBeVisible();
   });
 
-  it('should open record details when click on item', () => {
+  it('should redirect to record details when click on item', () => {
     renderRouting(false);
 
     const inputSearch = screen.getByRole('searchbox', { name: /searchFieldLabel/ });
     const localCheckbox = screen.getByRole('checkbox', { name: /sources.local/ });
-    const item = screen.getByRole('link', { name: /A journey through Europe/ });
+    const item = screen.getByRole('link', { name: /Record title 1/ });
 
     user.type(inputSearch, 'test');
     user.click(localCheckbox);
     user.click(item);
 
     expect(mutator.query.update).toHaveBeenCalled();
-    // expect(screen.getByRole('textbox', { name: /test/ }));
+  });
+
+  it('should open record details', () => {
+    renderRouting(false, '/view/3');
+
+    expect(screen.getByTestId('search-instance-details')).toBeVisible();
+    expect(screen.getByText('Contributors')).toBeVisible();
   });
 });
