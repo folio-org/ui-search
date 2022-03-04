1512702c71b680fdf47b2d68a2e132b6f88f6838
UladzislauKutarkin <72550466+UladzislauKutarkin@users.noreply.github.com>
2022-03-04 16:27:11 +0300

    Release v5.1.0 (#282)

--- test/jest/babel.config.js
+++ test/jest/babel.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react'],
+  presets: ['@babel/preset-env', ['@babel/preset-react', {
+    'runtime': 'automatic'
+  }]],
   plugins: [
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
